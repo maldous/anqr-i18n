d7f836b348d49f96c439f26532daccf813ec9ad8
Matthew Aldous <matthew.aldous@gmail.com>
2026-01-02 09:54:38 +1100

    1767269582

--- template-about.ts
+++ template-about.ts
@@ -1,5 +1,5 @@
-import type { PageDefinition } from '../types'
-import { LAST_UPDATED, CONTACT_EMAIL } from '../types'
+import type { PageDefinition } from '../types';
+import { CONTACT_EMAIL, LAST_UPDATED } from '../types';
 
 export const about: PageDefinition = {
   title: '1',
@@ -8,63 +8,35 @@ export const about: PageDefinition = {
   sections: [
     {
       heading: '3',
-      paragraphs: [
-        '4',
-        '5',
-      ],
+      paragraphs: ['4', '5'],
     },
     {
       heading: '6',
-      bullets: [
-        '7',
-        '8',
-        '9',
-        '10',
-        '11',
-        '12',
-        '501',
-      ],
+      bullets: ['7', '8', '9', '10', '11', '12', '501'],
     },
     {
       heading: '13',
-      paragraphs: [
-        '14',
-        '502',
-        '15',
-        '16',
-      ],
+      paragraphs: ['14', '502', '15', '16'],
     },
     {
       heading: '17',
-      paragraphs: [
-        '18' + CONTACT_EMAIL + '19',
-      ],
+      paragraphs: [`18${CONTACT_EMAIL}19`],
     },
     {
       heading: '20',
-      paragraphs: [
-        '21',
-        '22',
-        '23',
-      ],
+      paragraphs: ['21', '22', '23'],
     },
     {
       heading: '24',
-      paragraphs: [
-        '25',
-      ],
+      paragraphs: ['25'],
     },
     {
       heading: '26',
-      paragraphs: [
-        '27',
-      ],
+      paragraphs: ['27'],
     },
     {
       heading: '28',
-      paragraphs: [
-        '29',
-      ],
+      paragraphs: ['29'],
     },
   ],
 };
